ee192f86355b8f749bc25f193dc54cad4563e78b
Vincent No <ghzmdr@gmail.com>
2018-02-23 21:57:57 +0100

    [=] Rework dev

--- commands/start-dev.sh
+++ commands/start-dev.sh
@@ -2,4 +2,4 @@ if ! [ -e ./volumes/data/dpkg_status ]; then
 	ln -s /var/lib/dpkg/status ./volumes/data/dpkg_status;
 fi;
 
-docker-compose -f docker-compose.yml up --build
+docker-compose up --build
