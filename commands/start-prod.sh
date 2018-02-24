0b32b0c5f065792f459c07c76d413eb08e1eef49
Vincent No <ghzmdr@gmail.com>
2018-02-24 13:37:19 +0100

    [+] Detach but keep logging on prod

--- commands/start-prod.sh
+++ commands/start-prod.sh
@@ -1,2 +1,3 @@
 cp /var/lib/dpkg/status ./volumes/data/dpkg_status;
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build;
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d;
+docker-compose logs -f -t;
